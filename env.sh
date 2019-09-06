b9a07f115352d19bac54c77af2fa6bf98c3e62d6
desertsniper87 <torshobuet@gmail.com>
2019-09-07 00:07:05 +0600

    environment variable setter

--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@
 
 export CLASSPATH=".:antlr-4.7.1-complete.jar:$CLASSPATH"
 
-alias grun='java -Xmx500M -cp "antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig
+alias grun='java -Xmx500M -cp "antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
 alias antlr4='java -Xmx500M -cp "antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
